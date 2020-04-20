5c1b465162c482a69da0a6bebf88b1cfe6f1f1eb
Smith <kory.smith@kroger.com>
2020-04-20 19:13:50 -0400

    Complete(?) promiseAll

--- Ch.11/promiseAll.js
+++ Ch.11/promiseAll.js
@@ -1,34 +1,39 @@
+
 function Promise_all(promises) {
   return new Promise((resolve, reject) => {
     let results = [];
     let counter = promises.length;
     if (promises.length == 0) resolve(promises);
-    for (let promise of promises) {
-      promise.then(result => {
-        results.push(result);
-        console.log({results});
+    promises.map((promise, i) => {
+      try {
+      promise.then((result) => {
+        results[i] = result;
+        // console.log({ results });
         counter--;
-        console.log(`Promises left: ${counter}`);
-      })
-      if (counter == 0) resolve(results);
-    }
-  }
-  );
+        // console.log(`Promises left: ${counter}`);
+        if (counter == 0) resolve(results);
+      })}
+      catch(e) {reject(e)};
+    })
+  });
 }
 
 // Test code.
-/* Promise_all([]).then(array => {
+Promise_all([]).then(array => {
   console.log("This should be []:", array);
-}); */
+});
+
 function soon(val) {
   return new Promise(resolve => {
     setTimeout(() => resolve(val), Math.random() * 500);
   });
 }
+
 Promise_all([soon(1), soon(2), soon(3)]).then(array => {
   console.log("This should be [1, 2, 3]:", array);
 });
-/* Promise_all([soon(1), Promise.reject("X"), soon(3)])
+
+Promise_all([soon(1), Promise.reject("X"), soon(3)])
   .then(array => {
     console.log("We should not get here");
   })
@@ -37,4 +42,3 @@ Promise_all([soon(1), soon(2), soon(3)]).then(array => {
       console.log("Unexpected failure:", error);
     }
   });
- */
